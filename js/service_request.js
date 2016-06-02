ba700b10ac7d2e507ff89ba87d373ca7a4235386
Tiago Andre <tiago.andre@medialog.pt>
2016-06-02 18:18:09 +0100

    add area to drop-target added service_request to callapi and generate request

--- js/service_request.js
+++ js/service_request.js
@@ -1,3 +1,31 @@
-app.factory('Request', function(data) {
-  debugger;
+app.factory('apiCall', function($http, $q) {
+  var jsonClonned = [];
+  function cleanArray(json) {
+    debugger;
+  $.extend(true, jsonClonned, json);
+
+  for (var i = 0; i < jsonClonned.length; i++) {
+  cleanArrayObject(jsonClonned[i], false);
+  }
+
+  console.log(json);
+
+  console.log(jsonClonned);
+  }
+
+  function cleanArrayObject(obj, removeType) {
+    delete obj.position;
+    if (removeType) {
+      delete obj.type;
+    }
+    delete obj.id;
+
+    for (var i = 0; i < obj.elements.length; i++) {
+      cleanArrayObject(obj.elements[i], true);
+    }
+  }
+
+  return {
+      cleanArray: cleanArray
+  };
 });
